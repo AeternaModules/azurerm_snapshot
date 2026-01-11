bf8b14965b7f207ba4396727c2783169d66c2369
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:22:22 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "snapshots" {
+  description = "All snapshot resources"
+  value       = azurerm_snapshot.snapshots
+}
+
---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "snapshots" {
+  description = <<EOT
+Map of snapshots, attributes below
+Required:
+    - create_option
+    - location
+    - name
+    - resource_group_name
+Optional:
+    - disk_access_id
+    - disk_size_gb
+    - incremental_enabled
+    - network_access_policy
+    - public_network_access_enabled
+    - source_resource_id
+    - source_uri
+    - storage_account_id
+    - tags
+    - encryption_settings (block):
+        - disk_encryption_key (required, block):
+            - secret_url (required)
+            - source_vault_id (required)
+        - key_encryption_key (optional, block):
+            - key_url (required)
+            - source_vault_id (required)
+EOT
+
+  type = map(object({
+    create_option                 = string
+    location                      = string
+    name                          = string
+    resource_group_name           = string
+    disk_access_id                = optional(string)
+    disk_size_gb                  = optional(number)
+    incremental_enabled           = optional(bool, false)
+    network_access_policy         = optional(string, "AllowAll")
+    public_network_access_enabled = optional(bool, true)
+    source_resource_id            = optional(string)
+    source_uri                    = optional(string)
+    storage_account_id            = optional(string)
+    tags                          = optional(map(string))
+    encryption_settings = optional(object({
+      disk_encryption_key = object({
+        secret_url      = string
+        source_vault_id = string
+      })
+      key_encryption_key = optional(object({
+        key_url         = string
+        source_vault_id = string
+      }))
+    }))
+  }))
+}
+
